3fc46cb4e00324a1dbf1762adb9669ccedb4ffb0
Vic Degraeve <vicdegraeve@gmail.com>
2018-03-21 20:24:02 +0100

    Models

---
+++ db/migrate/20180321191617_create_players.rb
@@ -0,0 +1,10 @@
+class CreatePlayers < ActiveRecord::Migration
+  def change
+    create_table :players do |t|
+      t.string :code
+      t.boolean :alive
+      t.timestamps null: false
+      t.references :games
+    end
+  end
+end
